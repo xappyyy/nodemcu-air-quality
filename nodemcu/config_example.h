c74d8d04ce022b5ebbc64b46e2273946f5af0b22
xappyyy <surin123.james@gmail.com>
2024-05-23 07:56:26 +0700

    chore: add config example

---
+++ nodemcu/config_example.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <Arduino.h>
+
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+#define MQTT_SERVER "mqtt.netpie.io"
+#define MQTT_PORT 1883
+#define MQTT_CLIENT ""
+#define MQTT_SECRET ""
+#define MQTT_TOKEN ""
+
+#endif
